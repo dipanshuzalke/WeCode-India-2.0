4424c47ffbf9b08f2830418b301487b0456f4bdf
Dipanshu Zalke <dipanshuzalke@gmail.com>
2026-06-22 03:41:52 +0530

    build

--- next.config.js
+++ next.config.js
@@ -7,15 +7,4 @@ module.exports = {
       },
     ],
   },
-
-  webpack: (config) => {
-    config.resolve = config.resolve || {};
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      os: false,
-    };
-    return config;
-  },
 };
